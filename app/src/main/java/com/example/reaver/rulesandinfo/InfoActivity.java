9127d78155f4e66de9051253f9dbd01311027752
Reaver <bs.stadnyk@gmail.com>
2014-07-10 21:26:20 +0300

    Add title, logo and "Rules" and "Info" activities

---
+++ app/src/main/java/com/example/reaver/rulesandinfo/InfoActivity.java
@@ -0,0 +1,15 @@
+package com.example.reaver.rulesandinfo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Reaver on 10.07.2014.
+ */
+public class InfoActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info);
+    }
+}
---
+++ app/src/main/java/com/example/reaver/rulesandinfo/RulesActivity.java
@@ -0,0 +1,15 @@
+package com.example.reaver.rulesandinfo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Reaver on 10.07.2014.
+ */
+public class RulesActivity extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rules);
+    }
+}
